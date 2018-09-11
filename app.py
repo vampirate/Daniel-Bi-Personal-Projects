99b4a082de1243792598b93e1faa21d0d9132ed3
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-11 15:21:22 +1000

    testing c

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def DogOrCat():
 @app.route("/getimageurl", methods = ['POST'])
 def getimageurl():
     url = request.form['imageurl']
-    ans = subprocess.check_output(["python", "static/cnn/CNN.py", url])
+    ans = subprocess.check_output(["python", "static/cnn/c.py", url])
     return jsonify(ans.decode("utf-8"))
     
 
