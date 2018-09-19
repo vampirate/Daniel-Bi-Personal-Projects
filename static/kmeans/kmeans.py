5e838b2b63c097d7a348d336d611b1c67d0c6ddb
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-19 10:51:41 +1000

    KMEANS WORKS NOW

--- static/kmeans/kmeans.py
+++ static/kmeans/kmeans.py
@@ -1,25 +1,44 @@
 import numpy as np
 import random
 import math
+import time
 
-k = 3
+########################################################################################
+# initiating the coordinates
+########################################################################################
+
+k = 2
+n = 8
+r = 5
 np.random.seed(0)
 
-x = np.random.permutation(10)
-y = np.random.permutation(10)
+x = np.random.permutation(n)
+y = np.random.permutation(n)
 coordinates = []
+cluster = []
 
-for count in range(10):
-    coordinates.append([x[count], y[count]])
+for count in range(n):
+    coordinates.append([x[count], y[count], [1]])
 #print(coordinates)
 
-xc = np.random.permutation(10)
-yc = np.random.permutation(10)
-#centeroid = []
+coordinates = [[1,2,1], [1,1,1], [10,2,1], [10,1,1], [10,10,1], [13,9,1], [1, 9,1], [1, 12,1]]
+
+########################################################################################
+# initiating the centroids
+########################################################################################
+
+xc = np.random.permutation(n)
+yc = np.random.permutation(n)
+centeroid = []
 
 for count in range(k):
     centeroid.append([xc[count], yc[count]])
-print(centeroid)
+#print("original centeroid is")
+#print(centeroid)
+
+########################################################################################
+# initiating the functions
+########################################################################################
 
 def getDistance(point1, point2):
     distanceX = abs(point1[0] - point2[0])
@@ -35,5 +54,71 @@ def findClosestCenteroid(point):
     #print(distance)
     return distance.index(min(distance))
 
-c = findClosestCenteroid([8, 9])
-print(c)
+########################################################################################
+# assigning coordinates to their closest centroids
+########################################################################################
+
+def assignCoordinatesToCentroids():
+    for count in range(len(coordinates)):
+        coordinates[count][2] = findClosestCenteroid(coordinates[count])
+        
+    #print("Points are")
+    #print(coordinates)
+
+########################################################################################
+# grouping the pooints into clusters
+########################################################################################
+
+def groupingIntoClusters():
+    for count in range(k):
+        cluster.append([])
+
+    for count2 in range(len(coordinates)):
+        for count in range(k):
+            if (coordinates[count2][2] == count):
+                cluster[count].append(coordinates[count2])
+
+########################################################################################
+# getting centroid for each cluster
+########################################################################################
+
+def updateCentroids():
+    for count in range(k):
+        sumX = 0
+        sumY = 0
+        for point in cluster[count]:
+            sumX = sumX + point[0]
+            sumY = sumY + point[1]
+        #print(sump)
+        centeroid[count] = [sumX / len(cluster[count]), sumY / len(cluster[count])]
+    #print("New centeroids are")
+    #print(centeroid)
+
+
+
+
+def main():
+    assignCoordinatesToCentroids()
+    groupingIntoClusters()
+    updateCentroids()
+    
+
+for count in range(6):
+    cluster = []
+    main()
+
+    if count == 5:
+        print("new clusters are")
+        print(cluster)
+
+
+    
+
+
+
+
+
+
+
+
+
