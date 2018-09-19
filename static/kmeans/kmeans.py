5f333aa8a1fac0463d8e26826f3f05742032f2d5
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-19 14:57:19 +1000

    eVERYTHING WORKS

--- static/kmeans/kmeans.py
+++ static/kmeans/kmeans.py
@@ -2,26 +2,27 @@ import numpy as np
 import random
 import math
 import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
 ########################################################################################
 # initiating the coordinates
 ########################################################################################
 
-k = 2
-n = 8
-r = 5
-np.random.seed(0)
+k = 3
+n = 200
+r = 30
+plt.ion()
 
+np.random.seed(2)
 x = np.random.permutation(n)
 y = np.random.permutation(n)
 coordinates = []
-cluster = []
+clusters = []
 
 for count in range(n):
-    coordinates.append([x[count], y[count], [1]])
+    coordinates.append([x[count], y[count], 1])
 #print(coordinates)
-
-coordinates = [[1,2,1], [1,1,1], [10,2,1], [10,1,1], [10,10,1], [13,9,1], [1, 9,1], [1, 12,1]]
 
 ########################################################################################
 # initiating the centroids
@@ -33,7 +34,6 @@ centeroid = []
 
 for count in range(k):
     centeroid.append([xc[count], yc[count]])
-#print("original centeroid is")
 #print(centeroid)
 
 ########################################################################################
@@ -45,13 +45,11 @@ def getDistance(point1, point2):
     distanceY = abs(point1[1] - point2[1])
     distance = math.sqrt(distanceX ** 2 + distanceY ** 2)
     return distance
-    #print(distance)
 
 def findClosestCenteroid(point):
     distance = []
     for count in range(k):
         distance.append(getDistance(point, centeroid[count]))
-    #print(distance)
     return distance.index(min(distance))
 
 ########################################################################################
@@ -61,9 +59,6 @@ def findClosestCenteroid(point):
 def assignCoordinatesToCentroids():
     for count in range(len(coordinates)):
         coordinates[count][2] = findClosestCenteroid(coordinates[count])
-        
-    #print("Points are")
-    #print(coordinates)
 
 ########################################################################################
 # grouping the pooints into clusters
@@ -71,12 +66,12 @@ def assignCoordinatesToCentroids():
 
 def groupingIntoClusters():
     for count in range(k):
-        cluster.append([])
+        clusters.append([])
 
     for count2 in range(len(coordinates)):
         for count in range(k):
             if (coordinates[count2][2] == count):
-                cluster[count].append(coordinates[count2])
+                clusters[count].append(coordinates[count2])
 
 ########################################################################################
 # getting centroid for each cluster
@@ -86,39 +81,88 @@ def updateCentroids():
     for count in range(k):
         sumX = 0
         sumY = 0
-        for point in cluster[count]:
+        for point in clusters[count]:
             sumX = sumX + point[0]
             sumY = sumY + point[1]
-        #print(sump)
-        centeroid[count] = [sumX / len(cluster[count]), sumY / len(cluster[count])]
-    #print("New centeroids are")
-    #print(centeroid)
+        centeroid[count] = [sumX / len(clusters[count]), sumY / len(clusters[count])]
 
+########################################################################################
+# main algorith
+########################################################################################
 
-
-
-def main():
+def update():
     assignCoordinatesToCentroids()
     groupingIntoClusters()
     updateCentroids()
+
+########################################################################################
+# drawing the clusters
+########################################################################################
+
+def draw():
+    print("Iteration: " + str(count))
     
+    printflag = 1
+    for countY in range(n, -1, -1):
+        for countX in range(n):
+            for cluster in clusters:
+                for point in cluster:
+                    if (point[0] == countX) & (point[1] == countY):
+                        for center in range(k):
+                            if point[2] == center:
+                                print(center, end=" ")
+                            printflag = 2
+            if printflag == 1:        
+                print("_", end=" ")
+            else:
+                printflag = 1
+        print("")
+    print(" ")
 
-for count in range(6):
-    cluster = []
-    main()
+def draw2():
+    xpoints = []
+    ypoints = []
+    cpoints = []
+    xcpoints = []
+    ycpoints = []
 
-    if count == 5:
-        print("new clusters are")
-        print(cluster)
+    for cluster in clusters:
+        for point in cluster:
+            xpoints.append(point[0])
+            ypoints.append(point[1])
+            cpoints.append(point[2])
 
-
+    for point in centeroid:
+        xcpoints.append(point[0])
+        ycpoints.append(point[1])
     
+    plt.scatter(xpoints, ypoints, c=cpoints)
+    plt.scatter(xcpoints, ycpoints, c="r")
+    plt.draw()
+    plt.pause(1)
+    plt.clf()
 
+########################################################################################
+# show the points
+########################################################################################
 
+def showPoints():
+    for cluster in clusters:
+        for point in cluster:
+            print(point)
 
+def showCentroids():
+    for point in centeroid:
+        print(point)
 
+########################################################################################
+# main function
+########################################################################################
 
-
+for count in range(r):
+    clusters = []
+    update()
+    draw2()
 
 
 
