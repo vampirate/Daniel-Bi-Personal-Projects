a0ff580445e78466a386b7d19cf42d3dc64bbd5b
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-20 11:38:47 +1000

    a

--- static/kmeans/kmeans.py
+++ static/kmeans/kmeans.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random, math, copy, time, sys
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
